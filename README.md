f7e1683516c930c381fe67db57f84b447e859463
Turlough <turloughcowman@gmail.com>
2020-06-16 22:56:05 +0100

    Refactored and added README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Horse racing results scraper
+Scrapes a betting site for racing results, and puts them into a data frame.
+
+I know nothing about horse racing. 
+This is more a project to get data for a friend,
+who has a sure-fire formula for winning at the horses.
+
+I guess we need data for this...
+
+
+
--- scraper.py
+++ scraper.py
@@ -6,24 +6,26 @@ from tabulate import tabulate
 
 
 cols = ['venue', 'horse', 'pos', 'total', 'odds-d', 'odds-f']
-master_df = pd.DataFrame(columns = cols)
+
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 main_url = 'https://www.timeform.com/horse-racing/results/2020-06-13/'
-page = requests.get(main_url, headers=headers)
 
 
-soup = BeautifulSoup(page.content, 'html.parser')
-# list of races for the day, we need to find the hrefs to the race details
-results_titles = soup.find_all('a', class_='results-title')
+def get_day_summary(url):
+
+	page = requests.get(url, headers=headers)
+
+	soup = BeautifulSoup(page.content, 'html.parser')
+	# list of races for the day, we need to find the hrefs to the race details
+	summaries = soup.find_all('a', class_='results-title')
+	urls = map(lambda x: 'https://timeform.com' + x['href'], summaries)
+	return urls
 
 
+def get_races(url):
 
-for t in results_titles:
-
-	# load the details for the race
-	url = 'https://timeform.com' + t['href']
 	details_page = requests.get(url, headers=headers)
 	soup = BeautifulSoup(details_page.content, 'html.parser')
 
@@ -32,7 +34,7 @@ for t in results_titles:
 
 	
 	# parse the URL for race details
-	race = t['href'].split('/')
+	race = url.split('/')
 	print('\n', race[-5], race[-4], race[-3])
 
 	# locate sequential listing of horses, in the order of finishing
@@ -43,6 +45,7 @@ for t in results_titles:
 
 	position = 0
 	for h in horses:
+
 		d = {}
 		d['odds-d'] = float(odds_dec[position].text)
 		d['odds-f'] = odds_frac[position].text
@@ -50,17 +53,32 @@ for t in results_titles:
 		d['pos'] = position
 		d['total'] = len(horses)
 		d['venue'] = '|'.join(race[-5:-2])
-		# strip the horse's number from the name
+		# Cleanup the horse's name
 		d['horse'] = re.split('\s', h.text, 1)[1]
-
 		rows.append(d)
 	
 	# DataFrame
-	df = pd.DataFrame(rows, columns = cols)
-	master_df = master_df.append(df, ignore_index = True)
-	print(tabulate(df, headers = cols, tablefmt='psql', showindex = False))
+	return pd.DataFrame(rows, columns = cols)
 
-print()
-print('\n ALL RESULTS\n')	
 
-print(tabulate(master_df, headers = cols, tablefmt='psql', showindex = True))
+def main():
+
+	master_df = pd.DataFrame(columns = cols)
+	urls = get_day_summary(main_url)
+	for url in urls:
+		df = get_races(url)
+		master_df = master_df.append(df, ignore_index = True)
+
+		print(tabulate(df, headers = cols, tablefmt='psql', showindex = False))
+
+	print()
+	print('\n ALL RESULTS\n')	
+
+	print(tabulate(master_df, headers = cols, tablefmt='psql', showindex = True))
+
+
+if __name__ == "__main__":
+
+	main()
+
+
